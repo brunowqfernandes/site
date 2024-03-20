adbf20374613824b8c6b67452b99b53191a29246
Guilherme Moura <guilhermermribeiro@gmail.com>
2024-03-20 12:58:51 -0300

    Etapa final do site

--- main.js
+++ main.js
@@ -15,9 +15,6 @@ window.onload = () => {
   console.log(Swiper)
   const swiper = new Swiper('.swiper-container', {
     loop: true,
-    autoplay: {
-      delay: 5000,
-    },
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
